7b4b79a1dc3a51557ce1597b4b3acb80dafdd14f
srininarnes <srininarne1@gmail.com>
2017-04-23 17:49:28 +0530

    new branch version 1.0

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -11,5 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "This is Sreenivas Narne!" );
         System.out.println( "Testing devops process" );
+        System.out.println( "devops professional" );
+        
     }
 }
